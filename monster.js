51ee9d4ee54528c6b11fc5bcd685b39a2ba2aeea
asg5704 <alexander.schmidt.garcia@gmail.com>
2018-03-16 12:21:51 -0400

    add health potion functionality

--- monster.js
+++ monster.js
@@ -2,31 +2,103 @@ new Vue({
     el: '#app',
     data: {
       newGame: false,
-      endOfGame: false,
       playerHealth: 100,
-      monsterHealth: 100
+      monsterHealth: 100,
+      turns: []
       },
     methods: {
+      startGame: function() {
+        this.newGame = true;
+        this.playerHealth = 100;
+        this.monsterHealth = 100;
+      },
       attack: function() {
-        let $this = this;
-
-        return
-          $this.monster.health -= 10,
-          $this.player1.health -= 8
-
+        //Player Damage Dealt
+        var damage = this.calcDamage(3, 9);
+        this.monsterHealth -= damage;
+        this.turns.unshift({
+          isPlayer: true,
+          isHealing: false,
+          text: `Player hits Monster ${damage}`
+        })
+        if(this.whoWon()) {
+          return;
+        }
+        //Monster Damage Dealt
+        this.monsterAttacks();
+        this.whoWon();
       },
       specialAtk: function() {
-        let $this = this;
-        return
-          $this.monster.health - 35,
-          $this.player1.health - 24
+        //Player Damage Dealt
+        let damage = this.calcDamage(13, 29);
+        this.monsterHealth -= damage;
+        this.turns.unshift({
+          isPlayer: true,
+          isHealing: false,
+          text: `Player hits Monster Hard ${damage}`
+        })
+        if(this.whoWon()) {
+          return;
+        }
+        //Monster Damage Dealt
+        this.monsterAttacks();
+        this.whoWon();
       },
       heal: function() {
-        let $this = this;
-        $this.player1.health += 15
+        if(this.playerHealth <= 90) {
+          this.playerHealth += 10;
+        } else {
+          this.playerHealth = 100;
+        }
+        this.turns.unshift({
+          isPlayer: true,
+          isHealing: true,
+          text: `Player drank a health potion +10`
+        })
+        this.monsterAttacks();
       },
       giveUp: function() {
-        endOfGame = true;
+        this.newGame = false;
+      },
+      calcDamage: function(min, max) {
+        return Math.max(Math.ceil(Math.random() * max), min);
+      },
+      whoWon: function() {
+        if(this.monsterHealth <= 0) {
+          if(confirm('You won! New Game?')) {
+            this.startGame();
+          } else {
+            this.newGame = false;
+          }
+          return true;
+        }
+        if(this.playerHealth <= 0) {
+          if(confirm('You lost! New Game?')) {
+            this.startGame();
+          } else {
+            this.newGame = false;
+          }
+          return true;
+        }
+        return false;
+      },
+      checkLevel: function(monHealth, pHealth) {
+        if(this.monHealth <= 60 || this.pHealth <= 60) {
+          //Change background color to yellow
+        }
+
+        if(this.monHealth <= 30 || this.pHealth <= 30) {
+          //Change color to red
+        }
+      },
+      monsterAttacks: function() {
+        let damage = this.calcDamage(5, 10);
+        this.playerHealth -= damage;
+        this.whoWon();
+        this.turns.unshift({
+          isPlayer: false,
+          text: `Monster hits Player ${damage}`
+        })
       }
       }
 });
